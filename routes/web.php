351893845d55ec1a16664a381a02d4d8c574ad99
Bertho <bertho_joris@yahoo.co.id>
2023-06-26 17:22:27 +0700

    Use laravel-crud-generator

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+	Route::prefix('roles')->group(function () {
+		Route::get('/', [App\Http\Controllers\RoleController::class, 'index'])->name('roles.index');
+		Route::get('/create', [App\Http\Controllers\RoleController::class, 'create'])->name('roles.create');
+		Route::post('/store', [App\Http\Controllers\RoleController::class, 'store'])->name('roles.store');
+		Route::post('/destroy', [App\Http\Controllers\RoleController::class, 'destroy'])->name('roles.destroy');
+		Route::get('/show/{id}', [App\Http\Controllers\RoleController::class, 'show'])->name('roles.show');
+		Route::get('/edit/{id}', [App\Http\Controllers\RoleController::class, 'edit'])->name('roles.edit');
+		Route::post('/update/{id}', [App\Http\Controllers\RoleController::class, 'update'])->name('roles.update');
+	});
+});
