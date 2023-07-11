470d95bbb6906b825005007fb5f56346ec18c6c0
Bertho <bertho_joris@yahoo.co.id>
2023-07-11 15:25:57 +0700

    Update

---
+++ resources/views/partials/menus/current.blade.php
@@ -0,0 +1,77 @@
+<div id="kt_aside_menu" class="aside-menu my-4" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500">
+	<!--begin::Menu Nav-->
+	<ul class="menu-nav">
+		<li class="menu-item" aria-haspopup="true">
+			<a href="index.html" class="menu-link">
+				<i class="menu-icon flaticon-home"></i>
+				<span class="menu-text">Dashboard</span>
+			</a>
+		</li>
+		<li class="menu-item menu-item-submenu menu-item-open menu-item-here" aria-haspopup="true" data-menu-toggle="hover">
+			<a href="javascript:;" class="menu-link menu-toggle">
+				<i class="menu-icon flaticon-interface-8"></i>
+				<span class="menu-text">General</span>
+				<i class="menu-arrow"></i>
+			</a>
+			<div class="menu-submenu">
+				<i class="menu-arrow"></i>
+				<ul class="menu-subnav">
+					<li class="menu-item menu-item-parent" aria-haspopup="true">
+						<span class="menu-link">
+							<span class="menu-text">General</span>
+						</span>
+					</li>
+					<li class="menu-item" aria-haspopup="true">
+						<a href="layout/general/fluid-content.html" class="menu-link">
+							<i class="menu-bullet menu-bullet-dot">
+								<span></span>
+							</i>
+							<span class="menu-text">Fluid Content</span>
+						</a>
+					</li>
+					<li class="menu-item" aria-haspopup="true">
+						<a href="layout/general/minimized-aside.html" class="menu-link">
+							<i class="menu-bullet menu-bullet-dot">
+								<span></span>
+							</i>
+							<span class="menu-text">Minimized Aside</span>
+						</a>
+					</li>
+					<li class="menu-item" aria-haspopup="true">
+						<a href="layout/general/no-aside.html" class="menu-link">
+							<i class="menu-bullet menu-bullet-dot">
+								<span></span>
+							</i>
+							<span class="menu-text">No Aside</span>
+						</a>
+					</li>
+					<li class="menu-item menu-item-active" aria-haspopup="true">
+						<a href="layout/general/empty-page.html" class="menu-link">
+							<i class="menu-bullet menu-bullet-dot">
+								<span></span>
+							</i>
+							<span class="menu-text">Empty Page</span>
+						</a>
+					</li>
+					<li class="menu-item" aria-haspopup="true">
+						<a href="layout/general/fixed-footer.html" class="menu-link">
+							<i class="menu-bullet menu-bullet-dot">
+								<span></span>
+							</i>
+							<span class="menu-text">Fixed Footer</span>
+						</a>
+					</li>
+					<li class="menu-item" aria-haspopup="true">
+						<a href="layout/general/no-header-menu.html" class="menu-link">
+							<i class="menu-bullet menu-bullet-dot">
+								<span></span>
+							</i>
+							<span class="menu-text">No Header Menu</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</li>
+	</ul>
+	<!--end::Menu Nav-->
+</div>
